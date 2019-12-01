3e1744f8ce30185e8eae4470e95b9670f2485e33
Grin <grinefimov@gmail.com>
2019-12-01 23:33:36 +0300

    Add project files.

---
+++ TexasHoldemManagerBot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace TexasHoldemManagerBot
+{
+    internal class Program
+    {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("1069876513:AAHCGIcCg0-SV8pqD18GX9yJLi7YRjC39JU");
+
+        private static void Main(string[] args)
+        {
+            var me = Bot.GetMeAsync().Result;
+            Console.WriteLine(
+                $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+        }
+    }
+}
